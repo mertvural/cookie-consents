864e1160ce1682733b4f8f3803e176a1187caea4
mert vural <mertvural90@gmail.com>
2022-06-24 14:06:28 +0300

    css revisions

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,18 @@
         {{ cmd.title }}
       </h2>
       <p>
-        {{ cmd.description }}
-        <button
-          type="button"
-          class="consent_modal__btn-link"
-          @click="openedModal()"
-        >
-          Seçmeme izin ver
-        </button>
+        {{ cmd.description }}        
       </p>
       <div class="consent_modal__buttons">
         <button type="button" class="btn btn--secondary" @click="acceptAll()">
-          Hepsini kabul et
+          HEPSİNİ KABUL ET
         </button>
-        <button type="button" class="btn btn--primary" @click="rejectAll()">
-          Hepsini reddet
+        <button
+          type="button"
+          class="btn btn--primary"
+          @click="openedModal()"
+        >
+          AYARLAR
         </button>
       </div>
     </div>
@@ -63,27 +60,7 @@
                       @change="catAllChecked(item.value, item.enabled)"
                       :checked="item.cookies.some((x) => x.enabled === true)"
                       :disabled="item.readonly"
-                    />
-                    <svg
-                      class="is_checked"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 426.67 426.67"
-                    >
-                      <path
-                        d="M153.504 366.84c-8.657 0-17.323-3.303-23.927-9.912L9.914 237.265c-13.218-13.218-13.218-34.645 0-47.863 13.218-13.218 34.645-13.218 47.863 0l95.727 95.727 215.39-215.387c13.218-13.214 34.65-13.218 47.86 0 13.22 13.218 13.22 34.65 0 47.863L177.435 356.928c-6.61 6.605-15.27 9.91-23.932 9.91z"
-                      ></path>
-                    </svg>
-                    <svg
-                      class="is_unchecked"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 212.982 212.982"
-                    >
-                      <path
-                        d="M131.804 106.49l75.936-75.935c6.99-6.99 6.99-18.323 0-25.312-6.99-6.99-18.322-6.99-25.312 0L106.49 81.18 30.555 5.242c-6.99-6.99-18.322-6.99-25.312 0-6.99 6.99-6.99 18.323 0 25.312L81.18 106.49 5.24 182.427c-6.99 6.99-6.99 18.323 0 25.312 6.99 6.99 18.322 6.99 25.312 0L106.49 131.8l75.938 75.937c6.99 6.99 18.322 6.99 25.312 0 6.99-6.99 6.99-18.323 0-25.313l-75.936-75.936z"
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
+                    />                    
                   </div>
                 </div>
 
@@ -113,27 +90,7 @@
                         :value="item.name"
                         :disabled="item.readonly"
                         v-model="item.enabled"
-                      />
-                      <svg
-                        class="is_checked"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 426.67 426.67"
-                      >
-                        <path
-                          d="M153.504 366.84c-8.657 0-17.323-3.303-23.927-9.912L9.914 237.265c-13.218-13.218-13.218-34.645 0-47.863 13.218-13.218 34.645-13.218 47.863 0l95.727 95.727 215.39-215.387c13.218-13.214 34.65-13.218 47.86 0 13.22 13.218 13.22 34.65 0 47.863L177.435 356.928c-6.61 6.605-15.27 9.91-23.932 9.91z"
-                        ></path>
-                      </svg>
-                      <svg
-                        class="is_unchecked"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 212.982 212.982"
-                      >
-                        <path
-                          d="M131.804 106.49l75.936-75.935c6.99-6.99 6.99-18.323 0-25.312-6.99-6.99-18.322-6.99-25.312 0L106.49 81.18 30.555 5.242c-6.99-6.99-18.322-6.99-25.312 0-6.99 6.99-6.99 18.323 0 25.312L81.18 106.49 5.24 182.427c-6.99 6.99-6.99 18.323 0 25.312 6.99 6.99 18.322 6.99 25.312 0L106.49 131.8l75.938 75.937c6.99 6.99 18.322 6.99 25.312 0 6.99-6.99 6.99-18.323 0-25.313l-75.936-75.936z"
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
+                      />                      
                     </div>
                   </div>
                 </div>
@@ -158,7 +115,7 @@
               </button>
               <button
                 type="button"
-                class="btn btn--primary btn--last"
+                class="btn btn--secondary btn--last"
                 @click="saveSettings()"
               >
                 Ayarları kaydet
