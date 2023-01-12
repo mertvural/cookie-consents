9b19754a63e7c98f4864df76917b0a80ccea250d
MERT VURAL <mert.vural@ext.digiturk.com.tr>
2023-01-12 09:31:54 +0300

    css edits

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-//import "./assets/config.css"
+//import "./assets/config.css" //projenizin icinde olmalidir. ***build alinca bunu kapatin
 createApp(App).mount('#app_cookieconsent')
 
 export {App}
